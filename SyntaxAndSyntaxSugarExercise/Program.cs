34d4bf053e58ff1b18313f5333da4e214a82bdf5
Zach Padovano <zacharyaz10@gmail.com>
2023-04-13 08:46:05 -0700

    First commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "is greater or equal to nine";
+            //}
+           var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater or equal to nine";
+            Console.WriteLine(response);
+
         }
     }
 }
